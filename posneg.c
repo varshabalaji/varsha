a057fc6cd0c82920f763b82360764b9b8e05e17c
varshabalaji <37687555+varshabalaji@users.noreply.github.com>
2018-03-23 14:41:33 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("positive number");
+	}
+	else if(a<0)
+	{
+		printf("negative number");
+	}
+	else
+	{
+		printf("zero");
+	}
+	return 0;
+}
